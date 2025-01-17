34826505538b43cb1dca0a82fb279538b4ce4477
Alexandre Gavriilidis <alexandre.gavriilidis@etnic.be>
2025-01-17 10:52:38 +0100

    feat(products): Subscribe to observale<product[]> and render data

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../../models/product';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: Product[] = []
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.productService
+    .getProducts()
+    .subscribe(
+      data => this.products = data
+    )
+  }
 }
